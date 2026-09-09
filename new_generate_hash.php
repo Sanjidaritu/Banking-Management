e1baebe8735e95e61a36d341579fe544a6c8e08d
Sanjidaritu <108417708+Sanjidaritu@users.noreply.github.com>
2026-09-09 18:35:25 -0400

    new_generate_hash.php

---
+++ new_generate_hash.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "9001002005: " . password_hash('ENROLL9012', PASSWORD_DEFAULT) . "<br>";
+echo "9001002006: " . password_hash('ENROLL3456', PASSWORD_DEFAULT) . "<br>";
+echo "9001002007: " . password_hash('ENROLL7890', PASSWORD_DEFAULT) . "<br>";
+echo "9001002008: " . password_hash('ENROLL2468', PASSWORD_DEFAULT) . "<br>";
