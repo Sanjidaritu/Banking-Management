0656bda3d1fa91af7a69b549a08802043d9899db
Sanjidaritu <108417708+Sanjidaritu@users.noreply.github.com>
2026-09-10 14:44:30 -0400

    new_generate_hash.php

--- new_generate_hash.php
+++ new_generate_hash.php
@@ -4,3 +4,11 @@ echo "9001002005: " . password_hash('ENROLL9012', PASSWORD_DEFAULT) . "<br>";
 echo "9001002006: " . password_hash('ENROLL3456', PASSWORD_DEFAULT) . "<br>";
 echo "9001002007: " . password_hash('ENROLL7890', PASSWORD_DEFAULT) . "<br>";
 echo "9001002008: " . password_hash('ENROLL2468', PASSWORD_DEFAULT) . "<br>";
+
+echo "9001002011: " . password_hash('ENROLL4827', PASSWORD_DEFAULT) . "<br>";
+echo "9001002012: " . password_hash('ENROLL6153', PASSWORD_DEFAULT) . "<br>";
+echo "9001002013: " . password_hash('ENROLL7384', PASSWORD_DEFAULT) . "<br>";
+echo "9001002014: " . password_hash('ENROLL2965', PASSWORD_DEFAULT) . "<br>";
+echo "9001002015: " . password_hash('ENROLL8519', PASSWORD_DEFAULT) . "<br>";
+echo "9001002016: " . password_hash('ENROLL3742', PASSWORD_DEFAULT) . "<br>";
+echo "9001002017: " . password_hash('ENROLL5296', PASSWORD_DEFAULT) . "<br>";
