084ae1960394d55d2f0a5a38c044c6b302ed19a7
Sanjidaritu <108417708+Sanjidaritu@users.noreply.github.com>
2026-09-11 17:17:10 -0400

    database.js

--- database.js
+++ database.js
@@ -1,21 +1,13 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-
     host: process.env.MYSQLHOST,
-
     port: Number(process.env.MYSQLPORT),
-
     user: process.env.MYSQLUSER,
-
     password: process.env.MYSQLPASSWORD,
-
     database: process.env.MYSQLDATABASE,
-
     waitForConnections: true,
-
     connectionLimit: 10,
-
     queueLimit: 0
 });
 
