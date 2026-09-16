c237983bcd8ece5d5cdfc0e21ea19d1da4531218
Sanjidaritu <108417708+Sanjidaritu@users.noreply.github.com>
2026-09-16 15:47:13 -0400

    new_generate_hash.php

--- new_generate_hash.php
+++ new_generate_hash.php
@@ -19,4 +19,11 @@ echo "9001002023: " . password_hash('ENROLL2023', PASSWORD_DEFAULT) . "<br>";
 echo "9001002024: " . password_hash('ENROLL2024', PASSWORD_DEFAULT) . "<br>";
 echo "9001002025: " . password_hash('ENROLL2025', PASSWORD_DEFAULT) . "<br>";
 
+
+echo "ENROLL2026: " . password_hash('ENROLL2026', PASSWORD_DEFAULT) . PHP_EOL;
+echo "ENROLL2027: " . password_hash('ENROLL2027', PASSWORD_DEFAULT) . PHP_EOL;
+echo "ENROLL2028: " . password_hash('ENROLL2028', PASSWORD_DEFAULT) . PHP_EOL;
+echo "ENROLL2029: " . password_hash('ENROLL2029', PASSWORD_DEFAULT) . PHP_EOL;
+echo "ENROLL2030: " . password_hash('ENROLL2030', PASSWORD_DEFAULT) . PHP_EOL;
+
 ?>
